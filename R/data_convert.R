cfedf8fccce6b265ad8d686a555dd2518cac47b2
Tu Hu <UYHDK@leo-pharma.com>
2020-08-27 14:11:29 +0200

    update rds

--- R/data_convert.R
+++ R/data_convert.R
@@ -1,7 +1,7 @@
 # Convert DGE list (.xlsx) to RDS
 library(dplyr)
-DGE_publication_filename <- list.files("data/DGE_list_publication/", full.names = TRUE)
+DGE_publication_filename <- list.files("data/DGE_list_publication/", pattern = "xlsx", full.names = TRUE)
 DGE_publication <- lapply(DGE_publication_filename, function(x){
   DGE <- readxl::read_excel(x)})
-names(DGE) <- DGE_publication_filename %>% stringr::str_extract("(LS|NL)vs(NL|NN)")
+names(DGE_publication) <- DGE_publication_filename %>% stringr::str_extract("(LS|NL)vs(NL|NN)")
 saveRDS(DGE_publication, file = "data/DGE_list_publication/DGE_publication.rds")
