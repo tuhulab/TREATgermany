79be57d9d3d051602e90d41e96a21b4b2a34ad07
Tu Hu <UYHDK@leo-pharma.com>
2020-09-01 10:53:18 +0200

    update

--- R/data_convert.R
+++ R/data_convert.R
@@ -4,4 +4,11 @@ DGE_publication_filename <- list.files("data/DGE_list_publication/", pattern = "
 DGE_publication <- lapply(DGE_publication_filename, function(x){
   DGE <- readxl::read_excel(x)})
 names(DGE_publication) <- DGE_publication_filename %>% stringr::str_extract("(LS|NL)vs(NL|NN)")
+
+lapply(names(DGE_publication), 
+       function(name = ...){
+         df <- DGE_publication[[name]]
+         readr::write_csv(df, file.path("data", "DGE_list_publication", paste0("pool_", name, ".csv")))})
+
 saveRDS(DGE_publication, file = "data/DGE_list_publication/DGE_publication.rds")
+
